4743d5bc75c7061ff108ed7edb6d8f8b39d94261
Daniel Seuffer <authanram@gmail.com>
2021-11-19 03:15:52 +0100

    Fix inconsistent argument naming

--- src/Descriptor.php
+++ src/Descriptor.php
@@ -4,14 +4,16 @@ declare(strict_types=1);
 
 namespace Authanram\Generators;
 
+use Illuminate\Support\Stringable;
+
 abstract class Descriptor
 {
     public const PATTERN = '{{ %s }}';
 
     abstract public static function path(): string;
 
-    /** @return array<string> */
-    abstract public static function fill(Input $data): array;
+    /** @return array<string, Stringable|string> */
+    abstract public static function fill(Input $input): array;
 
     public static function pattern(): string
     {
